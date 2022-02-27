535cebf4b6985dcdd665f9f629221def658084c3
shirowanisan <shirowani333@gmail.com>
2022-02-27 13:44:35 +0900

    Add build script

---
+++ script.ps1
@@ -0,0 +1,15 @@
+python -m venv venv
+.\venv\Scripts\activate
+pip list
+pip install --upgrade pip setuptools
+pip install --no-deps -r requirements-coeiroink-no-deps.txt
+pip install -r requirements-coeiroink.txt
+pip install -r requirements-dev.in
+python generate_licenses.py > licenses.json
+pip install pyinstaller
+pyinstaller run.py
+mkdir dist/run/espnet
+cp venv/Lib/site-packages/espnet/version.txt dist/run/espnet/
+mkdir dist/run/librosa/util/example_data
+cp venv/Lib/site-packages/librosa/util/example_data/registry.txt dist/run/librosa/util/example_data/
+cp venv/Lib/site-packages/librosa/util/example_data/index.json dist/run/librosa/util/example_data/
