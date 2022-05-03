871e29333990fe278aa3b0f43cc527ef98caad9f
shirowanisan <shirowani333@gmail.com>
2022-05-03 19:17:01 +0900

    Fix for coeiroink-v.1.3.0-gpu

--- script.ps1
+++ script.ps1
@@ -5,6 +5,7 @@ pip install --upgrade pip setuptools
 pip install --no-deps -r requirements-coeiroink-no-deps.txt
 pip install -r requirements-coeiroink.txt
 pip install -r requirements-dev.in
+pip3 install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio===0.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 python generate_licenses.py > licenses.json
 pip install pyinstaller
 pyinstaller run.py
